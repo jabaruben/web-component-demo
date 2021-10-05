43c5c96ff776acbb39eb63b0e5443915ae998e44
Ruben Carracedo <jabaruben@gmail.com>
2021-10-05 18:27:53 +0200

    Adjust demo changes

--- components/MyWebComponent.js
+++ components/MyWebComponent.js
@@ -1,52 +1,61 @@
 class MyWebComponent extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({ mode: "open" });
-    }
-  
-    static get styles() {
-      return /* css */`
-        :host {
-          --desktop-background-color: #008282;
-        }
-        .wallpaper {
-          background: var(--desktop-background-color);
-          width: var(--resolution-width, 640px);
-          height: var(--resolution-height, 480px);
-          position: relative;
-          cursor: default
-        }
-        win95-start-bar {
-          position: absolute;
-          bottom: 0;
-        }
-        win95-start-menu {
-          position: absolute;
-          bottom: 30px;
-        }
-      `;
-    }
-  
-    enableBSOD() {
-      this.shadowRoot.querySelector(".wallpaper").innerHTML = /* html */`
-        <win95-bsod-screen></win95-bsod-screen>
-      `;
-    }
-  
-    connectedCallback() {
-      this.render();
-      // this.shadowRoot.querySelector("win95-start-bar").addEventListener("click", () => this.enableBSOD());
-    }
-  
-    render() {
-      this.shadowRoot.innerHTML = /* html */`
-      <style>${MyWebComponent.styles}</style>
-      <div class="wallpaper">
-        <win95-icon-manager></win95-icon-manager>
-        <win95-start-menu></win95-start-menu>
-        <win95-start-bar></win95-start-bar>
-      </div>`;
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
   }
-  
-  customElements.define("my-web-component", MyWebComponent);
+
+  static get styles() {
+    return /* css */`
+      :host {
+        --desktop-background-color: #008282;
+        --resolution-height: 180px;
+      }
+      .wallpaper {
+        background: var(--desktop-background-color);
+        width: var(--resolution-width, 640px);
+        height: var(--resolution-height, 480px);
+        position: relative;
+        cursor: default
+      }
+    `;
+  }
+
+  changedContent() {
+    this.shadowRoot.querySelector(".wallpaper").innerHTML = /* html */ `
+      <div>Mostramos un cambio de contenido</div>
+      <button btn-action="return">Volver</button>
+    `;
+    this.shadowRoot.querySelector('button[btn-action="return"]').addEventListener("click", () => this.originalContent());
+    this.showCambiado();
+  }
+
+  originalContent() {
+    this.shadowRoot.querySelector(".wallpaper").innerHTML = /* html */ `
+      <div>Mostramos el contenido original</div>
+      <button btn-action="change">Cambia el contenido</button>
+    `;
+    this.shadowRoot.querySelector('button[btn-action="change"]').addEventListener("click", () => this.changedContent());
+  }
+
+  showCambiado() {
+    const container = document.createElement("template");
+    container.innerHTML = /* html */ `
+      <div>cambiado</div>
+    `;
+    this.shadowRoot.appendChild(container.content.firstElementChild);
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = /* html */`
+      <style>${MyWebComponent.styles}</style>
+      <div class="wallpaper"></div>
+    `;
+    this.originalContent();
+  }
+}
+
+customElements.define("my-web-component", MyWebComponent);
